6afc212b8e920371c88df977561e146f444e6371
EsmondCho <chilver23@naver.com>
2018-03-18 19:22:41 +0900

    input coin info to DB

--- analyzer/input_coin.py
+++ analyzer/input_coin.py
@@ -2,13 +2,26 @@ from analyzer.models import *
 
 
 def input_coin_upbit():
-    coin_ko_name_list = []
-    coin_symbol_list = []
 
-    f = open("/Users/esmond/Desktop/coin_upbit", 'r')
+    f = open("/Users/esmond/Desktop/coin_upbit.txt", 'r')
     lines = f.readlines()
     for index, line in enumerate(lines):
-        print(line)
+        if index%10 == 2:
+            coin_name_ko = line.strip()
+        elif index%10 == 3:
+            print(index)
+            coin_symbol = line.split('/')[0]
+            coins = []
+            coins = CoNmTb.objects.filter(conm_symbol=coin_symbol)
+            if coins:
+                coins[0].conm_name_ko = coin_name_ko
+                coins[0].save()
+            else:
+                coin = CoNmTb.objects.create(
+                    conm_name_ko=coin_name_ko,
+                    conm_symbol=coin_symbol
+                )
+                coin.save()
 
     f.close()
 
@@ -19,29 +32,22 @@ def input_coin_coinmarketcap():
     f = open("/Users/esmond/Desktop/coin_coinmarketcap.txt", 'r')
     lines = f.readlines()
 
-    for line in lines:
+    for index, line in enumerate(lines):
+        print(index)
         coin_info_list = []
         coin_info_list = line.split("\t")
         coin_symbol = coin_info_list[2]
-        coin_eng_name = coin_info_list[1]
+        coin_name_eng = coin_info_list[1]
 
-        sym = CoSymTb.objects.create(
-            cosm_symbol = coin_symbol
+        if CoNmTb.objects.filter(conm_name_eng=coin_name_eng):
+            continue
+
+        coin = CoNmTb.objects.create(
+            conm_name_eng=coin_name_eng,
+            conm_symbol=coin_symbol
         )
-        sym.save()
-
-
-
-
-
-        # comment = CoTb.objects.create(
-        #     ps_id=post, co_content=content,
-        #     co_date=timezone.make_aware(co_date_obj)
-        # )
-        # comment.save()
-
-    # print(coin_symbol_list)
-    # print(coin_eng_name_list)
+        coin.save()
 
     f.close()
+
     return True
